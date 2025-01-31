d665e6f747a7a70eaa35e551b338afbd0bd058f7
Henrique <henriqueferreira0320@gmail.com>
2025-01-31 14:50:34 -0300

    Setup React Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Helmet, HelmetProvider } from "react-helmet-async"
 import { RouterProvider } from "react-router-dom"
 import "./index.css"
 import { router } from "./Routes"
 
 export function App() {
-    return <RouterProvider router={router} />
+    return (
+        <HelmetProvider>
+            <Helmet titleTemplate="%s | pizza.shop" />
+            <RouterProvider router={router} />
+        </HelmetProvider>
+    )
 }
