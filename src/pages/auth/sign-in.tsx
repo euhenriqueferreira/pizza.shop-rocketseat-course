7a305138cc16fc28b70636c58832a0f3000022a7
Henrique <henriqueferreira0320@gmail.com>
2025-01-31 15:28:21 -0300

    Sign In validation and Toast

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,8 +2,38 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Helmet } from "react-helmet-async"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import { z } from "zod"
+
+const signInForm = z.object({
+    email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+    } = useForm<SignInForm>()
+
+    async function handleSignIn(data: SignInForm) {
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+
+            toast.success("Enviamos um link de autenticação para seu e-mail.", {
+                action: {
+                    label: "Reenviar",
+                    onClick: () => handleSignIn(data),
+                },
+            })
+        } catch {
+            toast.error("Credenciais inválidas.")
+        }
+    }
+
     return (
         <>
             <Helmet title="Login" />
@@ -18,13 +48,24 @@ export function SignIn() {
                         </p>
                     </div>
 
-                    <form className="space-y-4">
+                    <form
+                        className="space-y-4"
+                        onSubmit={handleSubmit(handleSignIn)}
+                    >
                         <div className="space-y-2">
                             <Label htmlFor="email">Seu e-mail</Label>
-                            <Input id="email" type="email" />
+                            <Input
+                                id="email"
+                                type="email"
+                                {...register("email")}
+                            />
                         </div>
 
-                        <Button className="w-full" type="submit">
+                        <Button
+                            className="w-full"
+                            type="submit"
+                            disabled={isSubmitting}
+                        >
                             Acessar Painel
                         </Button>
                     </form>
